3b5a1a7808c8edc00c3ac09f2c4a166b661a7278
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-08-05 06:22:20 +0530

    Add files via upload

---
+++ SumofNnaturalnumbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class SumofNnaturalnumbers
+{
+	public static void main (String args []){
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int s = (n*(n+1))/2;
+		System.out.println(s);
+	}
+}
