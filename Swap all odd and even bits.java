9ac04afd30f291919bef834f01aa807b38531709
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-09-06 21:42:37 +0530

    Add files via upload

---
+++ Swap all odd and even bits.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+    //Function to swap odd and even bits.
+    public static int swapBits(int n) 
+    {
+	    // Your code
+	    
+	    int e = n & 0xAAAAAAAA;
+	    e = e >> 1;
+	    int o = n & 0x55555555;
+	    o = o << 1;
+	    return e|o;
+	}
+    
+}
+
+//{ Driver Code Starts.
+
+class BitWise{
+
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();//testcases
+		while(t-->0){
+		    int n = sc.nextInt();//taking n
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling swapBits() method of class
+		    //Swap
+		    System.out.println(obj.swapBits(n));
+		}
+	}
+}
+// } Driver Code Ends
